c29919894e11ef464ca9322aff5191248bff0cde
Gerysnz <gerardfuentesaf@gmail.com>
2026-04-07 17:05:58 +0200

    Primer commit proyecto Django

---
+++ biblio/admin.py
@@ -0,0 +1,49 @@
+from atexit import register
+from django.contrib import admin
+from django.utils.html import format_html
+
+
+# Register your models here.
+from .models import *
+
+
+    
+
+
+
+class ImatgeInline(admin.TabularInline):
+    model = Imatge
+    extra = 3
+    readonly_fields = ('vista_previa',) # Opcional: para ver la miniatura ahí mismo
+
+    def vista_previa(self, obj):
+        if obj.imatge:
+            return format_html('<img src="{}" width="100" height="100" style="object-fit: contain;" />', obj.imatge.url)
+        return "Sin imagen"
+    
+
+     
+class LlibreAdmin(admin.ModelAdmin):
+    inlines = [ImatgeInline]
+    # Afegeix el mètode a readonly_fields perquè es mostri en la pàgina d'edició
+    readonly_fields = ('vista_previa_imatge',)
+    
+    # Defineix quins camps es mostren en el formulari d'edició
+    #fields = ('nom', 'imatge', 'vista_previa_imatge')
+    exclude = ()
+    
+    def vista_previa_imatge(self, obj):
+        if obj.imatge:
+            # Retorna l'etiqueta HTML amb l'URL de la imatge
+            return format_html('<img src="{}" width="150" height="150" style="object-fit: cover;" />', obj.imatge.url)
+        return "No hi ha imatge"
+    
+    vista_previa_imatge.short_description = 'Previsualització'
+
+
+
+    
+admin.site.register(Llibre,LlibreAdmin)
+admin.site.register(Usuari)
+admin.site.register(Imatge)
+
---
+++ biblio/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+ 
+class Llibre (models.Model):
+    titol = models.CharField(max_length=100)
+    autor = models.CharField(max_length=200)
+    resum = models.TextField(null=True,blank=True)
+    data_edicio = models.DateField(null=True,blank=True)
+    imatge = models.ImageField(upload_to='imatges/', null=True, blank=True)
+
+    def __str__(self):
+        return self.titol + " - " + self.autor
+    
+class Usuari(AbstractUser):
+    auth_token = models.CharField(max_length=32,blank=True,null=True)
+
+
+# modelo imagen q puede ser de un libro.
+class Imatge(models.Model):
+    llibre = models.ForeignKey(Llibre, on_delete=models.CASCADE, related_name='imatges')
+    imatge = models.ImageField(upload_to='imatges/', null=True, blank=True)
+
+    def __str__(self):
+        return f"Imatge de {self.llibre.titol}"
